83c852104baaaffface560ddd73dc0b65b9e7f16
Giuseppe P <giuseppe.pias@gmail.com>
2019-12-05 11:51:41 +0100

    ADD db create and insert script

--- dbBilancioPersonale.py
+++ dbBilancioPersonale.py
@@ -6,10 +6,10 @@ from app.models.users import User
 db.drop_all()
 db.create_all()
 
-# admin = User(username='admin',email='admin@example.com')
-# guest = User(username='guest',email='guest@example.com')
+admin = User(username='admin',email='admin@example.com')
+guest = User(username='guest',email='guest@example.com')
 
-# db.session.add(admin)
-# db.session.add(guest)
-# db.session.commit()
+db.session.add(admin)
+db.session.add(guest)
+db.session.commit()
 
